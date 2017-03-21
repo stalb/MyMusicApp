a4a5d4c1ea664d6b89819e10e4f144d9cff7e9cf
Stephane Talbot <Stephane.Talbot@univ-savoie.fr>
2017-03-21 09:45:58 +0100

    Ajout de la gestions de boutons de volume par l'application

--- MyMusicAppp/app/src/main/java/fr/usmb/iutc/mmi/s4/mymusicappp/MainActivity.java
+++ MyMusicAppp/app/src/main/java/fr/usmb/iutc/mmi/s4/mymusicappp/MainActivity.java
@@ -63,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 restart();
             }
         });
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
     }
 
     @Override
